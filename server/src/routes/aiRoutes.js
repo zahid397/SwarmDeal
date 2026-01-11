c9d1c8ad1b1cb7cd90e1a3b054b1839694b4e775
zahid397 <zh05698@gmail.com>
2026-01-11 18:16:41 +0600

    Create aiRoutes.js

---
+++ server/src/routes/aiRoutes.js
@@ -0,0 +1,6 @@
+import express from 'express';
+import AgentController from '../controllers/agentController.js';
+const router = express.Router();
+
+router.post('/chat', AgentController.chat);
+export default router;
