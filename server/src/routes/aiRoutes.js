e9b99f3ade396d22819705b4c3faf006c64f9f0f
zahid397 <zh05698@gmail.com>
2026-01-11 19:51:21 +0600

    Update aiRoutes.js

--- server/src/routes/aiRoutes.js
+++ server/src/routes/aiRoutes.js
@@ -1,6 +1,10 @@
+// server/src/routes/aiRoutes.js
+
 import express from 'express';
 import AgentController from '../controllers/agentController.js';
+
 const router = express.Router();
 
 router.post('/chat', AgentController.chat);
+
 export default router;
