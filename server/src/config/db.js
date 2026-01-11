54b4d02ccf4a745f361c990e4e6f5325e708b01b
zahid397 <zh05698@gmail.com>
2026-01-11 20:44:11 +0600

    Update db.js

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,17 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
     console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error(`❌ DB Error: ${error.message}`);
+    console.error("❌ MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
+
 export default connectDB;
