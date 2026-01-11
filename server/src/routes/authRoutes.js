8037da87282dd2653bef915c009e585a9bc5853f
zahid397 <zh05698@gmail.com>
2026-01-11 19:06:24 +0600

    Update authRoutes.js

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import AuthController from '../controllers/authController.js';
+import AuthController from '../controllers/AuthController.js';
 import { authenticate } from '../middleware/authMiddleware.js';
+
 const router = express.Router();
 
 router.post('/login', AuthController.login);
 router.get('/me', authenticate, AuthController.getProfile);
+
 export default router;
