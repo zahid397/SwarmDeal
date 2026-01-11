a1a4cb0ee39bbb0afa238caf7d933252e01c68b2
zahid397 <zh05698@gmail.com>
2026-01-11 18:25:43 +0600

    Create axios.ts

---
+++ client/src/lib/axios.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+const api = axios.create({ baseURL: '/api', headers: { 'Content-Type': 'application/json' } });
+api.interceptors.request.use((config) => {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+export default api;
