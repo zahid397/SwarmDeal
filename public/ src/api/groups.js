926466600ec09493d77c7ecc2c427f287834bbb7
zahid397 <zh05698@gmail.com>
2026-01-12 01:34:17 +0600

    Delete public/ src directory

--- public/ src/api/groups.js
+++
@@ -1,5 +0,0 @@
-import api from './axios';
-
-export const createGroup = async (groupData) => {
-  return await api.post('/groups/create', groupData);
-};
--- public/ src/utils/constants.js
+++
@@ -1,1 +0,0 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
