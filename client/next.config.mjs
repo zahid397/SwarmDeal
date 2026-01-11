2409a91ff249bd58fe83e57a6bb9285565769736
zahid397 <zh05698@gmail.com>
2026-01-11 18:24:53 +0600

    Create next.config.mjs

---
+++ client/next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  async rewrites() {
+    return [{ source: '/api/:path*', destination: 'http://localhost:5000/api/:path*' }];
+  },
+};
+export default nextConfig;
